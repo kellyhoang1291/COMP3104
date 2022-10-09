e89c5039e788aa07b8bc1385b5ab23d561789596
kellyhoang1291 <diemphuong1291@gmail.com>
2022-10-09 14:15:20 -0400

    React Project code added

---
+++ build/precache-manifest.d8306a410da257b52785be0e4534c54a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58c0f0b5194b5982a450",
+    "url": "/kellyhoang1291/COMP3104/static/css/main.381b2f9d.chunk.css"
+  },
+  {
+    "revision": "58c0f0b5194b5982a450",
+    "url": "/kellyhoang1291/COMP3104/static/js/main.58c0f0b5.chunk.js"
+  },
+  {
+    "revision": "8a620aded09ab4cd4f6a",
+    "url": "/kellyhoang1291/COMP3104/static/js/runtime~main.8a620ade.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/kellyhoang1291/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "2437a7f7b9e359ca7b3cae315b36a730",
+    "url": "/kellyhoang1291/COMP3104/index.html"
+  }
+];
